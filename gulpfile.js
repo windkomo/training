3256d712f3b03f67aaa8a06cf7d263865f0f895c
Maxime Khoy <maxime.khoy@ionisx.com>
2016-02-20 10:43:57 +0100

    Clean gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
 var gulp             = require('gulp');
+
 var stylus           = require('gulp-stylus');
 var gutil            = require('gulp-util');
+
 var bootstrapStyl    = require('bootstrap-styl');
+
 var webpack          = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var webpackConfig    = require('./webpack.config.js');
+var webpackConfig    = require('./webpack.config');
 
-var paths = {
-    styles: ['assets/styles/**/*.styl'],
-};
+gulp.task('webpack-dev-server', function () {
+    var config = Object.create(webpackConfig);
 
-gulp.task('webpack-dev-server', function (c) {
-    // modify some webpack config options
-    var myConfig = Object.create(webpackConfig);
-    myConfig.devtool = 'eval';
-    myConfig.debug = true;
-    // Start a webpack-dev-server
-    new WebpackDevServer(webpack(myConfig), {
+    config.devtool = 'eval';
+    config.debug = true;
+
+    new WebpackDevServer(webpack(config), {
         hot: true,
         colors: true
     })
-    .listen(myConfig.devServer.port, 'localhost', function (err) {
+    .listen(config.devServer.port, 'localhost', function (err) {
         if (err) {
             throw new gutil.PluginError('webpack-dev-server', err);
         }
+
         gutil.log('[webpack-dev-server]', 'http://localhost:3333/index.html');
     });
 });
@@ -34,12 +34,10 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('./static/'));
 });
 
-// Rerun the task when a file changes
 gulp.task('watch', function() {
-    gulp.watch(paths.styles, ['styles']);
+    gulp.watch('assets/styles/**/*.styl', ['styles']);
 });
 
-// The default task (called when you run `gulp` from cli)
 gulp.task('default', [
     'watch',
     'webpack-dev-server',
